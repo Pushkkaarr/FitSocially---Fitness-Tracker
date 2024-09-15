49d25b47b8265af6000f0d97bdeaaa0b7e0d09ad
Pushkkaarr <pushkarmmhatre3090@gmail.com>
2024-09-15 12:31:26 +0530

    update : changes in usercontroller for email and password verification

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 import User from '../models/userSchema.js'
 import bcryptjs from'bcryptjs';
+import jwt from 'jsonwebtoken'
 
 dotenv.config();
 
@@ -45,6 +46,74 @@ export const registerUser = async(req,res)=>{
             })
         }
 }
+
+export const checkEmail = async(req,res)=>{
+    try {
+        const { email } = req.body
+
+        const checkEmail = await User.findOne({email}).select("-password")
+
+        if(!checkEmail){
+            return res.status(400).json({
+                message : "user not exit",
+                error : true
+            })
+        }
+
+        return res.status(200).json({
+            message : "email verify",
+            success : true,
+            data : checkEmail
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message : error.message || error,
+            error : true
+        })
+    }
+}
+
+export const checkPassword = async(req,res)=>{
+    try {
+        const { password, userId } = req.body
+
+        const user = await User.findById(userId)
+
+        const verifyPassword = await bcryptjs.compare(password,user.password)
+
+        if(!verifyPassword){
+            return res.status(400).json({
+                message : "Please check password",
+                error : true
+            })
+        }
+
+        const tokenData = {
+            id : user._id,
+            email : user.email 
+        }
+        const token = await jwt.sign(tokenData,process.env.JWT_SECRET_KEY,{ expiresIn : '1d'})
+
+        const cookieOptions = {
+            http : true,
+            secure : true
+        }
+
+        return res.cookie('token',token,cookieOptions).status(200).json({
+            message : "Login successfully",
+            token : token,
+            success :true
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message : error.message || error,
+            error : true
+        })
+    }
+}
+
 export const profile=async(req,res)=>{
     try {
         return res.json({
--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -3,10 +3,11 @@ import bodyParser from 'body-parser';
 import multer from 'multer';
 import path from 'path';
 //import userController from '../controller/userController.js';
-import { registerUser,caloriesCalculator, dietPlan, profile } from "../controller/userController.js";
+import { checkEmail,registerUser,caloriesCalculator, dietPlan, profile } from "../controller/userController.js";
 const router=express.Router();
 
 router.route("/register").post(registerUser)
+router.route("/checkEmail").post(checkEmail)
 router.route("/profile").get(profile)
 router.route("/calculate-calories").post(caloriesCalculator)
 router.route("/DietPlan").get(dietPlan)
