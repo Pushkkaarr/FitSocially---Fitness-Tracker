188c1f6593093c6c8be21fd6d5e3e9b75d9b7fb3
Pushkkaarr <pushkarmmhatre3090@gmail.com>
2024-09-25 17:17:23 +0530

    update : registration page with user photo working

---
+++ Frontend/fit social/src/helpers/uploadFile.js
@@ -0,0 +1,24 @@
+const cloudName = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+
+console.log('Cloud Name:', cloudName);
+const url = `https://api.cloudinary.com/v1_1/${cloudName}/auto/upload`;
+console.log('Upload URL:', url);
+
+// Make the upload request using the constructed URL
+
+const uploadFile = async(file)=>{
+    const formData = new FormData()
+    formData.append('file',file)
+    formData.append("upload_preset","chat-app-file")
+
+    const response = await fetch(url,{
+        method : 'post',
+        body : formData
+    })
+    const responseData = await response.json()
+
+
+    return responseData
+}
+
+export default uploadFile
