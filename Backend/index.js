15c79b4a388fe4ae0a0496766fa6886ae1af4c53
Pushkkaarr <pushkarmmhatre3090@gmail.com>
2024-12-19 14:19:05 +0530

    fix : CORS Error solved

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ app.use('/uploads', express.static('uploads'));
 // app.use(cors());
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend URL
+  origin: 'https://fit-socially-fitness-tracker.vercel.app/', // Replace with your frontend URL
   credentials: true // Allow credentials (cookies, authorization headers, etc.)
 }));
 
